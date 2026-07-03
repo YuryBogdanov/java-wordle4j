8e2708cf9b4c1679981d43437e5d73f09a875f85
Yury Bogdanov <yury-bogdanov@mail.ru>
2026-07-03 22:06:22 +0300

    code style fix

--- src/ru/yandex/practicum/logger/Logger.java
+++ src/ru/yandex/practicum/logger/Logger.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.logger;
 
 public interface Logger {
+
     void setupLogger() throws Exception;
     void logMessage(String message);
     void closeLogger() throws Exception;
